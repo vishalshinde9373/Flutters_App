460020786bddd72f51cc3cfe40c56ff17c1cd91c
vishalsantoshshinde <vishalsantoshshinde@gmail.com>
2024-10-03 01:01:54 +0530

    9combination push

---
+++ 9Combination_Column/flutter_application_6/lib/main.dart
@@ -0,0 +1,47 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp( 
+    const MainApp(),
+  );
+}
+
+class MainApp extends StatelessWidget{
+  const MainApp({super.key});
+  
+  @override 
+  Widget build(BuildContext context){
+    print("Device width:${MediaQuery.of(context).size.width}");
+    print("Device width:${MediaQuery.of(context).size.height}");
+    return MaterialApp( 
+      home: Scaffold(  
+        appBar: AppBar( 
+          title: const Text( 
+            "Coloum Demo",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Container( 
+          width: MediaQuery.of(context).size.width,
+          color: Colors.red,
+          child: Column( 
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [ 
+              Container( 
+                height: 250,
+                width: 200,
+                color: Colors.amber,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+}
